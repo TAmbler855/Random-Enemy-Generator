3d8d9936dd7f69f1e728b436770daebd804e96c1
unknown <tambler855@west-mec.org>
2018-02-16 18:23:03 -0700

    Fixed bug, and changed comments.

--- enemyFinder.js
+++ enemyFinder.js
@@ -9,21 +9,20 @@ function findEnemy(){
     var difficulty = document.getElementById('diff').value;
     var randomEnemy;
     
-    if(difficulty <= 3){//picks a random easy enemy if slider value is 3 or less
+    if(difficulty <= 3){//picks a random easy enemy if difficulty value is 3 or less
         randomEnemy = easyEnemies[Math.floor(Math.random() * easyEnemies.length)];
     }
-    else if(difficulty > 3 && difficulty <= 6){//picks a random medium enemy if slider value is between 4 and 6
+    else if(difficulty > 3 && difficulty <= 6){//picks a random medium enemy if difficulty value is between 4 and 6
         randomEnemy = mediumEnemies[Math.floor(Math.random() * mediumEnemies.length)];
     }
-    else if(difficulty > 6 && difficulty <= 9){//picks random hard enemy if slider value is between 7 and 9
+    else if(difficulty > 6 && difficulty <= 9){//picks random hard enemy if difficulty value is between 7 and 9
         randomEnemy = hardEnemies[Math.floor(Math.random() * hardEnemies.length)];
     }
-    else if(difficulty === 10){//picks random extreme enemy if slider value is 10
+    else if(difficulty = 10){//picks random extreme enemy if difficulty value is 10
         randomEnemy = extremeEnemies[Math.floor(Math.random() * extremeEnemies.length)];
     }
     else{
         window.alert('The difficulty value must be between 1 and 10');
-        load();
     }
     var foundEnemy = document.getElementById('foundEnemy').innerHTML = 'The party has encountered: ' + randomEnemy + '!';
 }
